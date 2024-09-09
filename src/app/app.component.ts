4e7a620a76bf4dd2327fd238432ac47174d3ccba
Bahnasawy <abdoelbahnsawy@yahoo.com>
2024-09-09 23:02:50 +0300

    client and employee Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../Components/sidebar/sidebar.component';
+import { ClientComponent } from "../Components/client/client.component";
+import { EmployeeComponent } from "../Components/employee/employee.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, ClientComponent, EmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { CompanyComponent } from '../Components/company/company.component';
 import { UnitsComponent } from '../Components/units/units.component';
+import { ClientComponent } from '../Components/client/client.component';
+import { EmployeeComponent } from '../Components/employee/employee.component';
 
 export const routes: Routes = [
     {path:"company",component:CompanyComponent,title:"Company"},
     {path:"unit",component:UnitsComponent,title:"Units"},
+    {path:"client",component:ClientComponent,title:"Clients"},
+    {path:"employee",component:EmployeeComponent,title:"Employees"},
 ];
