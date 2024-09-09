9150a3b9635d8dc020bb1bf336dafe1dcd33893b
Mohamed Moustafa <mo7ammed.doh@gmail.com>
2024-09-09 19:03:33 +0300

    add Sales invoice Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../Components/sidebar/sidebar.component';
+import { HomeComponent } from '../Components/home/home.component';
+import { SalesInvoiceComponent } from '../Components/sales-invoice/sales-invoice.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SidebarComponent],
+  imports: [RouterOutlet,SidebarComponent,HomeComponent,SalesInvoiceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SalesInvoiceComponent } from '../Components/sales-invoice/sales-invoice.component';
+import { HomeComponent } from '../Components/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'sales-invoice', component: SalesInvoiceComponent },
+];
