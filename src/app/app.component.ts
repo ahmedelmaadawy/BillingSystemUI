180cdc9e0774329ed24c60cfc49ce214c6645c16
Bahnasawy <abdoelbahnsawy@yahoo.com>
2024-09-10 18:42:53 +0300

    creating Unit form and editing unit list color and adding name atribute to form inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../Components/sidebar/sidebar.component';
 import { ClientComponent } from "../Components/client/client.component";
 import { EmployeeComponent } from "../Components/employee/employee.component";
+import { UnitFormComponent } from "../Components/unit-form/unit-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, ClientComponent, EmployeeComponent],
+  imports: [RouterOutlet, SidebarComponent, ClientComponent, EmployeeComponent, UnitFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { CompanyComponent } from '../Components/company/company.component';
 import { UnitsComponent } from '../Components/units/units.component';
 import { ClientComponent } from '../Components/client/client.component';
 import { EmployeeComponent } from '../Components/employee/employee.component';
+import { UnitFormComponent } from '../Components/unit-form/unit-form.component';
 
 export const routes: Routes = [
     {path:"company",component:CompanyComponent,title:"Company"},
     {path:"unit",component:UnitsComponent,title:"Units"},
     {path:"client",component:ClientComponent,title:"Clients"},
     {path:"employee",component:EmployeeComponent,title:"Employees"},
+    {path:"unit-form",component:UnitFormComponent,title:"UnitForm"},
 ];
