3273bba79995d98de62c0f4cbb32f392166c109a
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-09-11 18:52:57 +0300

    fixed all confilcts:

--- src/Components/sales-invoice/sales-invoice.component.ts
+++ src/Components/sales-invoice/sales-invoice.component.ts
@@ -1,29 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sales-invoice',
   standalone: true,
-  imports: [ReactiveFormsModule], 
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './sales-invoice.component.html',
-  styleUrls: ['./sales-invoice.component.css']
+  styleUrls: ['./sales-invoice.component.css'],
 })
 export class SalesInvoiceComponent {
-
   requestForm: FormGroup;
-
+  addedItems = [
+    {
+      code: '',
+      name: '',
+      quantity: '',
+      unit: '',
+      sellingPrice: '',
+      discount: '',
+      total: '',
+      balance: '',
+    },
+  ];
   constructor(private fb: FormBuilder) {
     this.requestForm = this.fb.group({
       employeeName: [''],
       date: [''],
       startTime: [''],
-      endTime: ['']
+      endTime: [''],
     });
   }
 
-  onSave() {
-    
-  }
+  onSave() {}
 
   resetForm() {
     this.requestForm.reset();
