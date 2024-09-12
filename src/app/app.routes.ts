9bc314b0e65f0d9e09ff9c05315c014259ecbd74
islam <ragaabislam@gmail.com>
2024-09-12 20:37:44 +0300

    storage report

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { CompanyComponent } from '../Components/company/company.component';
 import { UnitsComponent } from '../Components/units/units.component';
 import { TypesComponent } from '../Components/types/types.component';
 import { SalesreportComponent } from'../Components/salesreport/salesreport.component';
+import { StorageReportComponent } from '../Components/storage-report/storage-report.component';
 export const routes: Routes = [
     {path:"company",component:CompanyComponent,title:"Company"},
     {path:"unit",component:UnitsComponent,title:"Units"},
     {path:"types",component:TypesComponent,title:"Types"},
-    {path:"salesreport",component:SalesreportComponent,title:"Salesreport"}
+    {path:"salesreport",component:SalesreportComponent,title:"Salesreport"},
+    {path:"storage-report",component:StorageReportComponent,title:"StorageReport"}
 ];
