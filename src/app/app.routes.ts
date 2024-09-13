dd2eee17392acd42ec554725165582670bc89a56
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-09-13 12:28:38 +0300

    fix conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { CompanyComponent } from '../Components/company/company.component';
-import { UnitsComponent } from '../Components/units/units.component';
-import { SalesreportComponent } from '../Components/salesreport/salesreport.component';
-import { SidebarComponent } from '../Components/sidebar/sidebar.component';
-import { StorageReportComponent } from '../Components/storage-report/storage-report.component';
+import { CompanyComponent } from './Components/company/company.component';
+import { UnitsComponent } from './Components/units/units.component';
+import { SalesreportComponent } from './Components/salesreport/salesreport.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { StorageReportComponent } from './Components/storage-report/storage-report.component';
 export const routes: Routes = [
     {path:"company",component:CompanyComponent,title:"Company"},
     {path:"unit",component:UnitsComponent,title:"Units"},
     {path:"salesreport",component:SalesreportComponent,title:"Salesreport"},
     {path:"sidebar",component:SidebarComponent,title:"Sidebar"},
     {path:"storage-report",component:StorageReportComponent,title:"StorageReport"}
-];
+    ];
