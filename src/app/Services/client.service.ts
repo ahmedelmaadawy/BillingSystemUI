af49c3c028b1cbc9c8698ae1276dda237a622b68
islam <ragaabislam@gmail.com>
2024-09-16 15:30:52 +0300

    savingclientservices

--- src/app/Services/client.service.ts
+++ src/app/Services/client.service.ts
@@ -1,15 +1,75 @@
-import { Position } from './../../../node_modules/@types/estree/index.d';
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { IClient } from '../Models/Iclient'; // Adjust the path
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ClientService {
+
+//   private apiUrl = 'https://localhost:7156/api/Client';
+
+//   constructor(private http: HttpClient) { }
+
+//   AddClient(clientData: IClient): Observable<IClient> {
+//     return this.http.post<IClient>(this.apiUrl, clientData);
+//   }
+
+//   GetClients(): Observable<IClient[]> {
+//     return this.http.get<IClient[]>(this.apiUrl);
+//   }
+
+//   UpdateClient(id: number, clientData: IClient): Observable<IClient> {
+//     return this.http.put<IClient>(`${this.apiUrl}/${id}`, clientData);
+//   }
+
+//   DeleteClient(id: number): Observable<void> {
+//     return this.http.delete<void>(`${this.apiUrl}/${id}`);
+//   }
+// }
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IClient } from '../Models/IClient';
+
+// client.module.ts (or the relevant module file)
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientComponent } from '../Components/client/client.component';
+
+@NgModule({
+  imports: [CommonModule],
+})
+export class ClientModule { }
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+  private apiUrl = 'https://localhost:7156/api/Client';  // استبدل بالعنوان الصحيح للـ API
 
-  constructor(private http:HttpClient) { }
-  AddClient(clientData:any){
-    return  this.http.post("https://localhost:7156/api/Client",clientData);
+  constructor(private http: HttpClient) { }
+
+  // Get all clients
+  GetClients(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  // Add a new client
+  AddClient(client: IClient): Observable<IClient> {
+    return this.http.post<IClient>(this.apiUrl, client);
+  }
+
+  // Update an existing client
+  UpdateClient(id: number, client: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, client);
+  }
+
+  // Delete a client
+  DeleteClient(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
-// islam ragab
