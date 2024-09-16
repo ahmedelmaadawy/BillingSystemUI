3952e2a6fce7cfb1a10cb2a212f6e7b3a1ce6887
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-09-16 23:28:16 +0300

    added company list

---
+++ src/app/Components/companylist/companylist.component.ts
@@ -0,0 +1,47 @@
+import { ICompany } from './../../Models/ICompany';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CompanyServiceService } from '../../Services/company-service.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-companylist',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './companylist.component.html',
+  styleUrl: './companylist.component.css',
+})
+export class CompanylistComponent implements OnInit {
+  companies?: ICompany[];
+  constructor(private _companyService: CompanyServiceService) {}
+  ngOnInit(): void {
+    this._companyService.getAllCompanies().subscribe({
+      next: (response) => {
+        this.companies = response;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  deleteCompany(id: number) {
+    this._companyService.deleteCompany(id).subscribe({
+      next: (response) => {
+        Swal.fire({
+          title: 'Company Deleted Successfully',
+          icon: 'success',
+          confirmButtonText: 'OK',
+        });
+        this.companies = this.companies?.filter((c) => c.id != id);
+      },
+      error: (err) => {
+        console.log(err);
+        Swal.fire({
+          title: 'Error Happened',
+          icon: 'error',
+          confirmButtonText: 'OK',
+        });
+      },
+    });
+  }
+}
