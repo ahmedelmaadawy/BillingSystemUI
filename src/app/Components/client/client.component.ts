d9c6a30bbc6a27010d386f7839f99ffc6f7ee891
islam <ragaabislam@gmail.com>
2024-09-16 22:26:08 +0300

    client service cruds

--- src/app/Components/client/client.component.ts
+++ src/app/Components/client/client.component.ts
@@ -1,31 +1,100 @@
 import { ClientService } from './../../Services/client.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-import { response } from 'express';
+import { IClient } from '../../Models/IClient';
 
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
-constructor(private client:ClientService) {
+export class ClientComponent implements OnInit {
   
-  
-}
-clientForm:FormGroup=new FormGroup({
-  name:new FormControl('',[Validators.required , Validators.minLength(3), Validators.maxLength(20) ]),
-  address:new FormControl('',[Validators.required , Validators.minLength(3), Validators.maxLength(20) ]),
-  phoneNumber:new FormControl('',[Validators.required,Validators.pattern(/^01[0152][0-9]{8}$/)])
-  
-})
-AddClient(){
-  this.client.AddClient(this.clientForm.value).subscribe({
-    next:(response)=>{alert("Success")},
-    error:(response)=>{alert("Error")}
+  clients: IClient[] = [];  // To store the list of clients
+  clientId:number=0;
+  isupdate:boolean=false;
+  selectedClientId: number | null = null;  // To store client ID when updating
+
+  constructor(private clientService: ClientService) {
+   
+  }
+  clientForm:FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    address: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    phoneNumber: new FormControl('', [Validators.required, Validators.pattern(/^01[0152][0-9]{8}$/)])
   });
+  ngOnInit() {
+   
+    this.GetClients();  
+  }
+
+  // Add a new client
+  AddClient() {
+  
+      this.clientService.AddClient(this.clientForm.value).subscribe({
+        next: (response) => {
+          alert('Client added successfully');
+          this.GetClients();
+                 },
+        error: (error) => {
+          alert('Error adding client');
+        }
+      });
+  
+    }
+
+  GetClients() {
+    this.clientService.GetClients().subscribe(
+      {
+        next:(response)=>{
+          this.clients=response;
+      }
+      },
+    );
+  }
+  
+EditClient(client:any){
+  this.clientId=client.id;
+  this.isupdate=true;
+  this.clientForm.patchValue({
+    name: client.name,
+    address: client.address,
+    phoneNumber: client.phoneNumber
+  });
+};
+Update(){
+  this.clientService.UpdateClient(this.clientId,this.clientForm.value).subscribe({
+    next:(response)=>{
+      this.isupdate=false;
+     this.GetClients();
+    },
+    error:(error)=>{
+      console.log(error);
+      
+    }
+  })
+ 
+    
 }
+  // Delete a client
+  DeleteClient(clientId: number) {
+    this.clientService.DeleteClient(clientId).subscribe({
+      next: (response) => {
+        this.GetClients();
+               },
+      error: (error) => {
+        alert('Error adding client');
+      }
+    });
+  }
+
+  // // Reset the form and clear selected client ID
+  // ResetForm() {
+  //   this.clientForm.reset();
+  //   this.selectedClientId = null;
+  // }
 }
+
