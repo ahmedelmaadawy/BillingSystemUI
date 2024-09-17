3b9d8b390e0725714931ff3f3c8701146aa5f706
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-09-17 21:20:43 +0300

    edited

--- src/app/Services/company-service.service.ts
+++ src/app/Services/company-service.service.ts
@@ -1,7 +1,7 @@
+import { ICompany } from './../Models/ICompany';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ICompany } from '../Models/ICompany';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,6 @@ export class CompanyServiceService {
 
 
   GetCompanyById(id: number): Observable<ICompany>{
-
     return this.httpClient.get<ICompany>(
       `https://localhost:44357/api/Company/${id}`
     );
@@ -24,7 +23,10 @@ export class CompanyServiceService {
   addCompany(company: ICompany):Observable<ICompany> {
     return  this.httpClient.post<ICompany>(`https://localhost:44357/api/Company`,company);
    }
-  editCompany() { }
+  editCompany(id: number, company :ICompany):Observable<any>{
+
+    return this.httpClient.put<any>(`https://localhost:44357/api/Company/${id}`, company);
+  }
   deleteCompany(id: number) {
     return this.httpClient.delete<void>(`https://localhost:44357/api/Company/${id}`);
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,11 +14,12 @@ import { TypesComponent } from './Components/types/types.component';
 import { SalesreportComponent } from './Components/salesreport/salesreport.component';
 import { ItemComponent } from './Components/item/item.component';
 import { CompanylistComponent } from './Components/companylist/companylist.component';
+import { TypesListComponent } from './Components/types-list/types-list.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'sales-invoice', component: SalesInvoiceComponent },
-  { path: 'company', component: CompanyComponent, title: 'Company' },
+  { path: 'company/:id', component: CompanyComponent, title: 'Company' },
   {
     path: 'company-list',
     component: CompanylistComponent,
@@ -28,7 +29,9 @@ export const routes: Routes = [
   { path: 'unit', component: UnitsComponent, title: 'Units' },
   { path: 'client', component: ClientComponent, title: 'Clients' },
   { path: 'employee', component: EmployeeComponent, title: 'Employees' },
-  { path: 'types', component: TypesComponent, title: 'Types' },
+  { path: 'types/:id', component: TypesComponent, title: 'Types' },
+  { path: 'types-list', component: TypesListComponent, title: 'Types' },
+
   {
     path: 'salesreport',
     component: SalesreportComponent,
