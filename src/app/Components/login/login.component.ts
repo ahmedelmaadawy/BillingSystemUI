2258c610c726bcd145b4d437f3d1b32b2957c95d
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-09-21 12:16:53 +0300

    first security

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,26 +1,48 @@
+import { response } from 'express';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoginService } from '../../Services/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  LoginForm:FormGroup=new FormGroup({
-    userName: new FormControl ('',[
-      Validators.required,
-          //*/
-
-    ]),
-    password: new FormControl ('',[
+  constructor(private loginService: LoginService) {}
+  LoginForm: FormGroup = new FormGroup({
+    userName: new FormControl('', [
       Validators.required,
       //*/
-    ])
-  })
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      //*/
+    ]),
+  });
+  login() {
+    if (this.LoginForm.valid) {
+      this.loginService.onLogin(this.LoginForm.value).subscribe({
+        next: (response) => {
+          console.log(response);
+          localStorage.setItem('Token', response.token);
+          
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      });
+    } else {
+      alert("error");
+    }
+  }
 }
-
-
